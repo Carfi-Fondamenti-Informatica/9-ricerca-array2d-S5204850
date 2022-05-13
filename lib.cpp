69a1e2df1ef482f627ed00694c8e8a401aced760
S5204850 <100359998+S5204850@users.noreply.github.com>
2022-05-13 11:56:42 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -15,4 +15,32 @@ int posizione(char a[20][10], char b[20]){
     }
     return -1;
 }
+void init(char a[20][10]){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++){
+            a[j][i]=' ';
+        }
+    }
+}
+
+void init2(char b[20]){
+    for(int i=0; i<20; i++){
+        b[i]=' ';
+    }
+}
+
+void inserimento(char a[20][10]){
+    for(int i=0;i<10;i++) {
+        char c[20];
+        init2(c);
+        cin >> c;
+        for(int j=0;j<20;j++){
+            a[j][i]=c[j];
+        }
+    }
+}
+
+void inserimento2(char b[20]){
+    cin>>b;
+}
   
